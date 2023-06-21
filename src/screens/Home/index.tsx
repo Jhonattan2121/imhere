05e0b77250d44c7f752abdc7c3d682f2ac156c07
jhonattan ferri <jhoontutors@gmail.com>
2023-06-21 00:03:08 -0300

    Input de dados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import {styles} from './styles'
 
 export  function Home() {
@@ -6,6 +6,10 @@ export  function Home() {
     <View style={styles.container}>
     <Text  style={styles.eventName}> React  </Text>
     <Text  style={styles.eventNames}> Native </Text>
+    <TextInput 
+    placeholder="Nome do participante"
+    placeholderTextColor="#6B6B6B"
+    style={styles.input}/>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,18 +4,28 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#131016',
-        padding: 24
+        padding: 50
     },
     eventName: {
-      fontSize: 25,
+      fontSize: 35,
       fontWeight: 'bold',
       color: '#FDFCFE',
       marginTop: 48
     },
     eventNames: {
       color: '#6b6b6b',
-        fontSize: 25,
+        fontSize: 30,
         fontWeight: 'bold',
       
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16
+
     }
   })
+
