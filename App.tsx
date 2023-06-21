b2f9c622b05feb8db41db5aa2c19f6ff92c8f9ba
jhonattan ferri <jhoontutors@gmail.com>
2023-06-20 23:29:14 -0300

    Estilizando Elementos

--- App.tsx
+++ App.tsx
@@ -3,9 +3,26 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-    <Text key="1"> React  </Text>
-    <Text key="2"> Native </Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+    <Text key="1" style={{
+      color: '#FDFCFE',
+      fontSize: 30,
+      fontWeight: 'bold',
+      
+      marginTop: 48
+      
+    }}> React  </Text>
+    <Text key="2" style={{
+      color: '#6b6b6b',
+      fontSize: 25,
+      fontWeight: 'bold',
+    
+      marginTop: 48
+    }}> Native </Text>
     </View>
   )
 }
