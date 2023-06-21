200e218a6aad49d2f6893f97f55a3c83778a8552
jhonattan ferri <jhoontutors@gmail.com>
2023-06-20 23:47:53 -0300

    Organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,31 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from './src/screens/Home';
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    <Text key="1" style={styles.eventName}> React  </Text>
-    <Text key="2" style={styles.eventNames}> Native </Text>
-    </View>
+   <Home />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-  },
-  eventName: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: '#FDFCFE',
-    marginTop: 48
-  },
-  eventNames: {
-    color: '#6b6b6b',
-      fontSize: 25,
-      fontWeight: 'bold',
-    
-  }
-})
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
+    },
+    eventName: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      color: '#FDFCFE',
+      marginTop: 48
+    },
+    eventNames: {
+      color: '#6b6b6b',
+        fontSize: 25,
+        fontWeight: 'bold',
+      
+    }
+  })
